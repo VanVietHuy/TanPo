51bbe8b27af87499c942ed2ef703d655d958ba66
Van Viet Huy <vanviethuy2018@gmail.com>
2023-07-24 21:46:07 +0700

    example_one

---
+++ myScript/script.js
@@ -0,0 +1,29 @@
+function cLick() {
+  let inPut = document.getElementById("number");
+  let number = parseInt(inPut.value);
+
+  if (number < 10 || number > 20) {
+    document.getElementById("error").innerHTML = "ERROR";
+    document.getElementById("error").style.color = "red";
+  } else if (number > 10 || number < 20) {
+    document.getElementById("error").innerHTML = "SUCCESS";
+    document.getElementById("error").style.color = "green";
+  }
+
+  let count = 0;
+  let imgContainer = document.getElementById("imgContainer");
+  imgContainer.innerHTML = "";
+
+
+  showFlower = setInterval(function () {
+    if (count >= number) {
+        clearInterval(showFlower);
+        return
+    }
+    let image = document.createElement("img");
+    image.src = "../images/img.jpg";
+    image.className = "img";
+    imgContainer.appendChild(image);
+    count++;
+  }, 1000);
+}
