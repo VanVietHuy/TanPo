862366b20d635c8b0fb4d8342fe66b316985ebbe
Van Viet Huy <vanviethuy2018@gmail.com>
2023-07-27 19:50:37 +0700

    change js

--- myScript/script.js
+++ myScript/script.js
@@ -2,23 +2,19 @@ function cLick() {
   let inPut = document.getElementById("number");
   let number = parseInt(inPut.value);
 
-  if (number < 10 || number > 20) {
+  if (number < 10 || number > 20 || isNaN(number)) {
     document.getElementById("error").innerHTML = "ERROR";
     document.getElementById("error").style.color = "red";
-  } else if (number > 10 || number < 20) {
-    document.getElementById("error").innerHTML = "SUCCESS";
-    document.getElementById("error").style.color = "green";
-  }
-
+  } else {
   let count = 0;
   let imgContainer = document.getElementById("imgContainer");
   imgContainer.innerHTML = "";
-
-
+  
+  
   showFlower = setInterval(function () {
     if (count >= number) {
-        clearInterval(showFlower);
-        return
+      clearInterval(showFlower);
+      return
     }
     let image = document.createElement("img");
     image.src = "../images/img.jpg";
@@ -26,4 +22,5 @@ function cLick() {
     imgContainer.appendChild(image);
     count++;
   }, 1000);
+}
 }
